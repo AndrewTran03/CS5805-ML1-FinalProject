a2f02d781aedd96c6ddc4c3b7045ff3de432f744
AndrewTran03 <andrewtranva@gmail.com>
2023-10-25 21:51:50 -0400

    Modified Blog Build Script for Angular Front-End and ML Blog Post Integration

--- blog_build_script.sh
+++ blog_build_script.sh
@@ -1,3 +1,4 @@
+
 echo "|-------- Beginning of Running P1 Clean Up Script --------|"
 echo "Windows (Git Bash) Configuration for Angular Project"
 time ng build --output-path docs --base-href='//CS5805-ML1-FinalProject\'
