44ea02fed1ce0d28ff50299d0ca11ad75d9c7ef1
AndrewTran03 <andrewtranva@gmail.com>
2023-11-22 01:35:50 -0500

    Fixed 'echo' statements in Blog Building Script

--- blog_build_script.sh
+++ blog_build_script.sh
@@ -1,7 +1,7 @@
 
-echo "|-------- Beginning of Running P1 Clean Up Script --------|"
+echo "|-------- Beginning of Running Project Clean Up Script --------|"
 echo "Windows (Git Bash) Configuration for Angular Project"
 time ng build --output-path docs --base-href='//CS5805-ML1-FinalProject\'
 echo "Copying Blog Posts Into Angular Project Final Deployment Folder"
 time cp -r blog-posts/ docs/
-echo "|-------- End of Running P1 Clean Up Script --------|"
+echo "|-------- End of Running Project Clean Up Script --------|"
